7418082e9b8a5f13678f4ed0467b69d771db56c7
Cactus_ana <anastasiamoskalenko666@gmail.com>
2026-05-20 10:47:34 +0300

    Add Book model class with Id, Title, Author, Year, Genre, Status

---
+++ LibraryManager/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Status { get; set; }
+        public string DisplayStatus => Status == "Available" ? "✅ Доступна" : "📖 Выдана";
+    }
+}
